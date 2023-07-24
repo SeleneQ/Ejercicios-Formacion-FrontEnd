5f5c0fb1d0c4dfd07d6d90711347d8e342142809
lunaselenebebe <lunaselenebebe@hotmail.com>
2023-07-24 17:45:04 -0600

    clase03 completa

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -4,9 +4,39 @@ const btn = document.querySelector('[data-form-btn]');
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+      //backticks son las comillas invertidas
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task"> ${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content);
 };
 
                     //el click es un listener
 btn.addEventListener('click', createTask);
 
+
+/*<li class="card" data-task>
+            <div>
+              <i class="far fa-check-square icon"></i>
+              <span class="task">Hacer curso de git</span>
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>
+          </li>
+          <li class="card">
+          <div>
+          <i class="far fa-check-square icon"></i>
+          <span class="task">Hacer curso de linux</span>
+          </div>
+          <i class="fas fa-trash-alt trashIcon icon"></i>
+          </li> */
