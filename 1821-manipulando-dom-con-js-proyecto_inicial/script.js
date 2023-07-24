54e3e6f8f3ed66a826420965dc2028c70e021bf5
lunaselenebebe <lunaselenebebe@hotmail.com>
2023-07-24 15:40:47 -0600

    clase02 completada

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,11 +1,12 @@
 const btn = document.querySelector('[data-form-btn]');
     console.log(btn);
-
+                       // Arrow functions o funciones anonimas =>
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    console.log(input.value);
+};
 
                     //el click es un listener
-btn.addEventListener("click", function () {
-    const input = document.querySelector('[data-form-input]');
-         console.log(input.value);
-});
-
+btn.addEventListener('click', createTask);
 
